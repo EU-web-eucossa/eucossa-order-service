a251a4688fad3b557d7d2da092e33e270a281c3f
otienochris <otienochris98@gmail.com>
2022-11-09 09:10:12 +0300

    adding project structure. closes #1

--- proto/src/main/java/com/eucossa/Main.java
+++ proto/src/main/java/com/eucossa/Main.java
@@ -1,4 +1,6 @@
-package com.eucossa;public class Main {
+package com.eucossa;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
